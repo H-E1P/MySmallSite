5c948963d9b0f996ca5190b5d7f8f4366372d21b
H-E1P <xe1f.epii@gmail.com>
2024-11-16 11:03:00 +0900

    setup.py 업데이트

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='MySmallSite',
-    version='0.0.1',
+    version='0.0.2',
     description='MySmallSite Generater',
     author='h-e1p',
     author_email='xe1f.epii@gmail.com',
     url='https://github.com/h-e1p/MySmallSite',
     install_requires=[],
     packages=find_packages(exclude=[]),
-    keywords=['xelp', 'xelf', 'xe1p', 'xe1f', 'h-e1p', 'h-elp', 'h-eip', 'he1p', 'MySmallSite', 'mysmallsite', 'My Small Site', 'my small site', 'mys', '*.mys', '.mys', '*mys', 'mss'],
+    keywords=['xelp', 'xelf', 'xe1p', 'xe1f', 'h-e1p', 'h-elp', 'h-eip', 'he1p', 'MYSMALLSITE, 'MySmallSite', 'mysmallsite', 'My Small Site', 'my small site', 'mys', '*.mys', '.mys', '*mys', 'mss'],
     python_requires='>=3.5',
     package_data={},
     zip_safe=False,
